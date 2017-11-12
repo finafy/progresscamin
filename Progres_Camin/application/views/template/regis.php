0218a7dd3cc9909a78dd0e968ab8d838432a8a2e
DESKTOP-J1DNFS8\lenovo <hfdbch@gmail.com>
2017-11-13 04:44:48 +0700

    Login & Register view

---
+++ Progres_Camin/application/views/template/regis.php
@@ -0,0 +1,28 @@
+<br>
+<div class="container">
+  <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Nama</label>
+    <input type="name" class="form-control" id="exampleInputName1" aria-describedby="nameHelp" placeholder="Masukkan nama">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">NRP</label>
+    <input type="nrp" class="form-control" id="exampleInputNRP1" aria-describedby="nrpHelp" placeholder="Masukkan NRP">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Masukkan password">
+  </div>
+    <div class="form-group">
+    <label for="exampleInputPassword1">Konfirmasi Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Masukkan password lagi untuk validasi">
+  </div>
+  <button type="submit" class="btn btn-primary">Daftar</button>
+</form>
+</div> 
---
+++ Progres_Camin/application/views/user/regispage.php
@@ -0,0 +1,13 @@
+<style type="text/css">
+	body {
+
+    background-image: url('../../assets/images/gre.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-color: lightgray;
+  
+	}	
+</style>
+	
+
+
